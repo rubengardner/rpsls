9c399ac4b02edc9fcde0db7de4643db2a1075d92
rubengardner <rgardnercuesta@gmail.com>
2022-09-02 15:53:20 +0000

    Add logic deciding winner of the round
    Add JS function that displays the result of the round in the game area

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,3 +10,103 @@ document.addEventListener("DOMContentLoaded", function(){
            
 
 }})
+
+
+//Returns winner of between user and enemy
+function battle(weapon1, weapon2){
+    
+
+    if(weapon1 === weapon2){
+       document.getElementById('result').innerText="Play again!";
+       document.getElementById('result-text').innerText='DRAW';
+
+    }
+    //Weapon selection: ROCK
+    else if(weapon1 === 'rock' && weapon2 === 'paper'){
+       enemyWin(); 
+       document.getElementById('result').innerText='Paper covers Rock';
+    }
+    else if(weapon1 === 'rock' && weapon2 === 'scissor'){
+        userWin();
+        document.getElementById('result').innerText= 'Rock crushes Scissors';
+    }
+    else if(weapon1 === 'rock' && weapon2 === 'lizard'){
+        userWin();
+        document.getElementById('result').innerText= 'Rock crushes Lizard';
+    }
+    else if(weapon1 === 'rock' && weapon2 === 'spock'){
+        enemyWin();
+        document.getElementById('result').innerText='Spock vaporizes Rock';
+    }
+    //weapon selection: PAPER
+    else if(weapon1 === 'paper' && weapon2 === 'rock'){
+        userWin();
+        document.getElementById('result').innerText= 'Paper covers Rock';
+    }
+    else if(weapon1 === 'paper' && weapon2 === 'scissor'){
+        enemyWin();
+        document.getElementById('result').innerText='Scissors cuts Paper';
+    }
+    else if(weapon1 === 'paper' && weapon2 === 'lizard'){
+        enemyWin();
+        document.getElementById('result').innerText= 'Lizard eats Paper';
+    }
+    else if(weapon1 === 'paper' && weapon2 === 'spock'){
+        userWin();
+        document.getElementById('result').innerText= 'Paper disproves Spock';
+    }
+    // Weapon selection: SCISSOR
+    else if(weapon1 === 'scissor' && weapon2 === 'rock'){
+        enemyWin();
+        document.getElementById('result').innerText= 'Rock crushes Scissors';
+    }
+    else if(weapon1 === 'scissor' && weapon2 === 'paper'){
+        userWin();
+        document.getElementById('result').innerText= 'Scissors cuts Paper';
+    }
+    else if(weapon1 === 'scissor' && weapon2 === 'lizard'){
+        userWin();
+        document.getElementById('result').innerText= 'Scissors decapitates Lizard';
+    }
+    else if(weapon1 === 'scissor' && weapon2 === 'spock'){
+        enemyWin();
+        document.getElementById('result').innerText= 'Spock smashes Scissors';
+    }
+    // Weapon selection: LIZARD
+    else if(weapon1 === 'lizard' && weapon2 === 'rock'){
+        enemyWin();
+        document.getElementById('result').innerText='Rock crushes Lizard';
+    }
+    else if(weapon1 === 'lizard' && weapon2 === 'paper'){
+        userWin();
+        document.getElementById('result').innerText= 'Lizard eats Paper';
+    }
+    else if(weapon1 === 'lizard' && weapon2 === 'scissor'){
+        enemyWin();
+        document.getElementById('result').innerText= 'Scissors decapitates Lizard';
+    }
+    else if(weapon1 === 'lizard' && weapon2 === 'spock'){
+        userWin();
+        document.getElementById('result').innerText= 'Lizard poisons Spock';
+    }
+    //Weapon selection: SPOCK
+    else if(weapon1 === 'spock' && weapon2 === 'rock'){
+        userWin();
+        document.getElementById('result').innerText= 'Spock vaporizes Rock';
+    }
+    else if(weapon1 === 'spock' && weapon2 === 'paper'){
+        enemyWin();
+        document.getElementById('result').innerText= 'Paper disproves Spock';
+    }
+    else if(weapon1 === 'spock' && weapon2 === 'scissor'){
+        userWin();
+        document.getElementById('result').innerText='Spock smashes Scissors';
+    }
+    else if(weapon1 === 'spock' && weapon2 === 'lizard'){
+        enemyWin();
+        document.getElementById('result').innerText= 'Lizard poisons Spock';
+    }
+    else{
+        alert('Unknow variable')
+    }
+}
